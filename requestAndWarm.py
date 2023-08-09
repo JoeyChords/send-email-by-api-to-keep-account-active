52d416331ce335b8b451d33df49dc664ac82238d
Joe Baumgartel <josephbaumgartel@gmail.com>
2023-08-08 18:57:37 -0700

    First commit. Fully tested and working application.

---
+++ requestAndWarm.py
@@ -0,0 +1,62 @@
+import datetime, logging, time, requests
+from dotenv import dotenv_values
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+config = dotenv_values(".env")
+COLD_URL = config["COLD_URL"]
+EMAIL_FROM = config["EMAIL_FROM"]
+EMAIL_TO = config["EMAIL_TO"]
+SENDGRID_API_KEY = config["SENDGRID_API_KEY"]
+logging.basicConfig(filename="warmer.log", level=logging.INFO)
+timeStarted = datetime.datetime.now()
+logging.info(
+    timeStarted.strftime("%m/%d/%Y, %H:%M:%S")
+    + "   requestAndWarm started on "
+    + COLD_URL
+)
+alertMessageMailed = False
+
+while True:
+    timeOfRequest = datetime.datetime.now()
+    try:
+        r = requests.get(COLD_URL)
+        if r.status_code == 200:
+            alertMessageMailed = False
+            logging.info(
+                timeOfRequest.strftime("%m/%d/%Y, %H:%M:%S")
+                + "   "
+                + COLD_URL
+                + " returned status code "
+                + str(r.status_code)
+            )
+        else:
+            raise Exception()
+    except:
+        logging.exception(
+            timeOfRequest.strftime("%m/%d/%Y, %H:%M:%S")
+            + "   Status code "
+            + str(r.status_code)
+        )
+        if not alertMessageMailed:
+            message = Mail(
+                from_email=EMAIL_FROM,
+                to_emails=EMAIL_TO,
+                subject="Problem with " + COLD_URL,
+                html_content="<strong>Something is wrong with "
+                + COLD_URL
+                + "</strong><br><br><strong>Status code = "
+                + str(r.status_code)
+                + "</strong>",
+            )
+            try:
+                sg = SendGridAPIClient(SENDGRID_API_KEY)
+                response = sg.send(message)
+                logging.info("  Alert message sent with Sendgrid.")
+                logging.info("  Sendgrid status: " + str(response.status_code))
+                logging.info(response.body)
+                logging.info(response.headers)
+                alertMessageMailed = True
+            except Exception as e:
+                logging.info(e.message)
+    time.sleep(840)
